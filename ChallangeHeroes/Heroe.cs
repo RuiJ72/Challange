3eb3fa71d68992ad5c9e58043376935e1a54373a
RuiJ72 <rlagos900@gmail.com>
2025-12-25 08:54:07 -0300

    Add project files.

---
+++ ChallangeHeroes/Heroe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    // Class representing a hero
+    public class Hero
+    {
+        // Properties
+        public string Name { get; set; }
+        public string Type { get; set; }
+
+        // Constructor
+        public Hero(string name, string type)
+        {
+            Name = name;
+            Type = type.ToLower(); 
+        }
+
+        // Method to attack
+        public void Attack()
+        {
+            string attackMove;
+
+            // Determine attack based on hero type
+            switch (Type)
+            {
+                case "mage":
+                    attackMove = "magic";
+                    break;
+                case "warrior":
+                    attackMove = "sword";
+                    break;
+                case "monk":
+                    attackMove = "martial arts";
+                    break;
+                case "ninja":
+                    attackMove = "shuriken";
+                    break;
+                default:
+                    attackMove = "unknown attack";
+                    break;
+            }
+
+            // Display attack message
+            Console.WriteLine($"{Name} the {Type} attacked using {attackMove}!");
+        }
+    }
+}
---
+++ ChallangeHeroes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AdventureGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Adventure Game!");
+
+            // Ask user for hero details
+            Console.Write("Enter hero name: ");
+            string heroName = Console.ReadLine();
+
+            string heroType;
+            while (true)
+            {
+                Console.Write("Enter hero type (mage, warrior, monk, ninja): ");
+                heroType = Console.ReadLine().ToLower();
+
+                // Validate input
+                if (heroType == "mage" || heroType == "warrior" || heroType == "monk" || heroType == "ninja")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid type. Please enter one of: mage, warrior, monk, ninja.");
+                }
+            }
+
+            // Create hero instance
+            Hero playerHero = new Hero(heroName, heroType);
+
+            // Attack
+            playerHero.Attack();
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
